1f360247f5bacfe4e03120d4a31c5ed64304abfd
Shady <jerrymani33@gmail.com>
2017-12-18 10:53:56 +0530

    initialCommit

---
+++ logpup/src/main/java/com/nanotricks/logpup/L.java
@@ -0,0 +1,23 @@
+package com.nanotricks.logpup;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by user on 12/18/2017.
+ */
+
+public class L {
+    private static String TAG="LogPup";
+
+    public void d(String msg){
+        Log.d(TAG,msg);
+    }
+    public void e(String msg){
+        Log.d(TAG,msg);
+    }
+    public void t(Context con, String msg){
+        Toast.makeText(con,msg,Toast.LENGTH_LONG).show();
+    }
+}
